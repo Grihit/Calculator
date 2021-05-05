6c2c3c10d58b67d20dbf1362ca16995622a8cae3
Grihit <grihitbudhiraja12@gmail.com>
2021-05-05 18:09:21 +0530

    FInal changes

--- Calculator.js
+++ Calculator.js
@@ -1,11 +1,21 @@
 var exp = "";
 var ans = 0.0;
 var c = 0;
+var dot = 0;
+var dot2 = 0;
 function Reset(){
     document.getElementById("display").innerHTML = "";
+    document.getElementById("display-top").innerHTML = "";
     exp="";
+    dot = 0;
 }
 function Display(x){
+    if(x =='+' || x =='/' || x =='*' || x =='-' || x =='%')
+        dot = 0;
+    if(x == '.' && dot>0)
+        return;
+    if(x == '.')
+        dot++;
     document.getElementById("display").innerHTML += x;
     
 }
@@ -24,6 +34,12 @@ function Back(){
         document.getElementById("display").innerHTML += z[i];
 }
 function Calculate(x){
+    if(x =='+' || x =='/' || x =='*' || x =='-' || x =='%')
+        dot2 = 0;
+    if(x == '.' && dot2>0)
+        return;
+    if(x == '.')
+        dot2++;
     if(x == '%'){
         exp += "/100*";
         c=1;
@@ -34,7 +50,20 @@ function Calculate(x){
     }
 }
 function Answer(){
+    //console.log(exp);
     var ans = eval(exp);
-    exp = ans;
+    ans = +(Math.round(ans + "e+5")  + "e-5");
+    exp='';
+    exp += ans;
+    if(exp.includes('.')==true)
+    {
+        dot++;
+        dot2++;
+    }
+    if(isNaN(ans))
+        document.getElementById("display").innerHTML = 'Answer too big';
+    else{
+    document.getElementById("display-top").innerHTML = document.getElementById("display").innerHTML;
     document.getElementById("display").innerHTML = ans;
+    }
 }
